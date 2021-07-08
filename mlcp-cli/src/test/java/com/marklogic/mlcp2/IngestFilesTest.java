65f5392eef6e67087cae485d6cc0b63cb1c369a0
Rob Rudin <rob.rudin@marklogic.com>
2021-07-08 16:14:27 -0400

    More reorganizing

--- mlcp-cli/src/test/java/com/marklogic/mlcp2/IngestFilesTest.java
+++ mlcp-cli/src/test/java/com/marklogic/mlcp2/IngestFilesTest.java
@@ -7,9 +7,11 @@ public class IngestFilesTest extends AbstractTest {
     @Test
     void test() {
         Main.main(new String[]{
+            "ingestFiles",
+            "--host", "localhost",
+            "--port", "8003",
             "--username", "admin",
             "--password", "admin",
-            "ingestFiles",
             "--input_file_path", "data/csv/**/*.csv"
             //, "--input_file_path", "data/csv/customers1.csv"
         });
